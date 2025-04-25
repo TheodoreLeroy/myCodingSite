164fa5c8071c305020791527fa2ac19df713ec2a
TheodoreLeroy <hunganhyb0503@gmail.com>
2025-04-26 00:39:49 +0700

    Add bubbleSort algorithm file

---
+++ BubbleSort.py
@@ -0,0 +1,29 @@
+def main():
+    # step 1: user input array size
+    arrSize = inputArraySize()
+    # step 2: user input array element by array size
+    unsortArr = inputArrayElement(arrSize)
+    # step 3: display unsort array
+    DisplayArray(unsortArr)
+    # step 4: sort array by bubble sort
+    sortedArr = SortArrayBubbleSort(unsortArr)
+    # step 5: display sorted array
+    DisplayArray(sortedArr)
+    return
+
+
+def inputArraySize():
+    x = 1
+    data = {x,
+            }
+    return
+
+
+def inputArrayElement(arrSize):
+    return
+
+def DisplayArray(unsortArr):
+    return
+
+def SortArrayBubbleSort(unsortArr):
+    return sortedArr
