87175abc1e7d019be2c7280a48dd70da97c2df41
TheodoreLeroy <hunganhyb0503@gmail.com>
2025-04-28 00:45:17 +0700

    add new BubbleSort file

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,29 +1,76 @@
-def main():
+
+
+
+def inputArraySize():
+    """_summary_
+        For user input array size
+    Returns:
+        int: array size
+    """
+    return int(input("Enter array size:"))
+# end def
+
+
+def inputArrayElement(arrSize):
+    """_summary_
+        For user input each index value
+    Args:
+        arrSize (int): size of array
+        array (list): list to store value
+    Returns:
+        list: an array with user input value
+    """
+    array = []
+    # loop to access each index of array and assign value to it
+    for i in range(arrSize):
+        array.append(input(f"Enter value for index {i}: "))
+    return array
+
+def DisplayArray(array, msg):
+    """_summary_
+        A function to display the array
+    Args:
+        array (list): the unsorted or sorted array
+        msg (_type_): messenger from server
+    """
+    print(msg)
+    # loop to access each index of array and display it value
+    for value in array:
+        print(value, end=" ")
+    print()
+    pass
+
+def SortArrayBubbleSort(unsortArr):
+    # loop to reset the inner loop, each reset one highest value is sorted
+    
+        # loop to access each value in the array
+        for i in range(len(unsortArr)-1):
+            # compare current value with next value
+            if (unsortArr[i] < unsortArr[i+1]):
+                # chua doi duoc vi tri!
+                """
+                Unsorted array:
+                7 12 9 11 3
+                1
+                9 12
+                3
+                3 11
+                """
+                print(i)
+                tmp = unsortArr[i]
+                unsortArr[i] = unsortArr[i+1]
+                unsortArr[i+1] = tmp
+                print(unsortArr[i], unsortArr[i+1])
+        return unsortArr
+
+if __name__ == "__main__":
     # step 1: user input array size
     arrSize = inputArraySize()
     # step 2: user input array element by array size
     unsortArr = inputArrayElement(arrSize)
     # step 3: display unsort array
-    DisplayArray(unsortArr)
+    DisplayArray(unsortArr, "Unsorted array: ")
     # step 4: sort array by bubble sort
     sortedArr = SortArrayBubbleSort(unsortArr)
     # step 5: display sorted array
-    DisplayArray(sortedArr)
-    return
-
-
-def inputArraySize():
-    x = 1
-    data = {x,
-            }
-    return
-
-
-def inputArrayElement(arrSize):
-    return
-
-def DisplayArray(unsortArr):
-    return
-
-def SortArrayBubbleSort(unsortArr):
-    return sortedArr
+    DisplayArray(sortedArr, "Sorted array: ")
