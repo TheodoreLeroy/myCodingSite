93b618d8e991d4f9dad2abea56342df74152a535
TheodoreLeroy <hunganhyb0503@gmail.com>
2025-05-08 06:42:04 +0700

    add uncomplete Quick Sort algorithm file

---
+++ Quick_Sort/QuickSort.py
@@ -0,0 +1,78 @@
+import random
+
+def inputSizeOfArray():
+    """Function for user to input size of array
+
+    Returns:
+        Integer: size of the array
+    """
+    return int(input("Enter number of array:"))
+# end def
+
+def generateRandomArray(size):
+    array = []
+    # loop to access each index of the array
+    for i in range(size):
+        array.append(random.randint(0, size))
+    return
+# end def
+
+def displayArray(array, msg):
+    """
+    Purpose: Function to display the array sequently
+    """
+    print(msg)
+    # loop to access each value from array and display its
+    for value in array:
+        print(value, "", end=" ")
+    print()
+    return
+# end def
+
+def sortArrayByQuickSort(unsortedArray):
+    """
+    Purpose: Function to sort array by quick sort algorithm using recursive
+    """
+    # i move from first one (increase), j move from last one (decrease)
+    # base case: i > j (stop)
+    # Find a pivot
+    """Beginning of partition: 
+    - move i until find value equal or greater than pivot value
+    - move j until find value equal or lower than pivot value
+    - compare value of i and value of j if i <= j then swap
+    """
+    def partition(unsortedArray):
+        # Find a pivot
+        pivot = int(len(unsortedArray))/2
+        quickSort(unsortedArray, unsortedArray[0], len(unsortedArray)-1, pivot)
+    def quickSort(array, first, last, pivot):
+        # Compare first and last
+        if first > last:
+            return
+        # Compare value of first and last
+        if array[first] >= pivot >= array[last]:
+            swapValue(array, first, last)
+            quickSort(array, first+1, last, pivot)
+            quickSort(array, first, last-1, pivot)
+
+
+
+    def swapValue(array, index1, index2):
+        array[index1], array[index2] = array[index2], array[index1]
+
+    partition(unsortedArray)
+    return unsortedArray
+# end def
+
+if __name__  == "__main__":
+    # step 1: user input size of array
+    arraySize = inputSizeOfArray()
+    # step 2: generate random value by array size
+    unsortedArray = generateRandomArray(arraySize)
+    unsortedArray = [1, 12, 5, 26, 7, 14, 3, 7, 2]
+    # step 3: display unsorted array
+    displayArray(unsortedArray, "Unsorted array: ")
+    # step 4: sort array by quick sort
+    sortedArray = sortArrayByQuickSort(unsortedArray)
+    # step 5: display sorted array
+    displayArray(sortedArray, "Sorted array: ")
